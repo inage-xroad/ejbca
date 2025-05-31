cf4faf2784ebffdf096c172efca2728a274cd65a
inage-xroad <xroad-inageip@proton.me>
2025-05-31 12:35:24 -0400

    Delete validate-https.sh

--- validate-https.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "[INFO] Testando conexão HTTPS no container EJBCA..."
-docker exec -it ejbca curl -k https://localhost:8443/ejbca/adminweb || echo "Erro: HTTPS não está escutando."
